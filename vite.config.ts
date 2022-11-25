c4e77e155fe80783c0a3b1af07b9a0b9d3945118
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-11-25 18:34:37 -0500

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
   build: {
-    target: 'esnext',
+    target: 'es2015',
   },
 });
