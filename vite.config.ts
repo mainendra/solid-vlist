ac9f8702a0649991a0d6d5c7832aa9095d6e00d8
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-11-25 19:25:56 -0500

    Updated port in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  server: {
+    // @ts-ignore
+    port: process.env.PORT || 3000,
+  },
   build: {
     target: 'es2015',
   },
