92491e7218152f494475c8e87b082a6608d654de
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-11-26 21:13:46 -0500

    Fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-// @ts-ignore
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  // @ts-ignore
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [solidPlugin()],
-    server: {
-        port: env.PORT || 3000,
-        host: true,
-    },
-    build: {
-        target: 'es2015',
-    },
-  }
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    // @ts-ignore
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        // vite config
+        define: {
+            lugins: [solidPlugin()],
+            server: {
+                port: env.PORT || 3000,
+                host: true,
+            },
+            build: {
+                target: 'es2015',
+            },
+        }
+    }
 })
