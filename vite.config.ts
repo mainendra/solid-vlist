8d6e3649e9b93879e064f47102adb828348b87ec
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-11-28 12:14:33 -0500

    Added legacy browsers support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import legacy from '@vitejs/plugin-legacy';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+      solidPlugin(),
+      legacy({
+          targets: ['defaults', 'not IE 11']
+      })
+  ],
   build: {
       target: 'es2015'
   }
